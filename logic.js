13ffd635edca02a4f7c704a6991f310ad74cc7eb
Jared Sanborn <jaredsanborn@gmail.com>
2020-12-08 23:29:54 -0600

    updated power descriptions

--- logic.js
+++ logic.js
@@ -1,12 +1,12 @@
 var powerDescriptions = [
-    'Water Echo',
-    'Steam Punch',
-    'Water Gun',
-    'Sonar',
-    'Matter State Change',
-    'Wet Teleport',
-    'Bubble Shield',
-    'Water Body',
+    'Mover: Can hop into water and then teleport out of the water within visual distance.',
+    'Shaker: Can control the state of water in an area.',
+    'Thinker: Can send out a mental pulse to sense water in a 50 ft radius.',
+    'Brute: Can create a water bubble around themselves to protect them. They are immersed and must hold their breath while using this.',
+    'Blaster: Water gun.',
+    'Master: Can create a water echo of him that follows a simple mental directive.',
+    'Striker: Can deal a blow to an enemy by collecting water on his hand and striking an enemy. As the enemy is hit, he turns the water into steam. (+1 to Brawn rolls for this ability)',
+    'Changer: Can turn a part of himself into water to cushion a blow. This can only be done as a reaction to a blow by instinct. This power cannot be activated on demand.'
 ];
 
 function SetRandomPower(powerSlot){
